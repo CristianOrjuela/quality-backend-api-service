9619a1448489c26405417ab9bda07be7f8d4c3c8
CristianOrjuela <cristian.orjuela@proviniti.com>
2023-04-29 12:20:21 -0500

    Establish the database connection

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app_server.set('port', process.env.PORT || 4000);
 // Middleware
 app_server.use(express.json());
 
+// Database connection
+const { db_connection } = require('./database/db_connection');
+
+try {
+
+  // Trying the database connection
+  const db_status = db_connection.authenticate();
+  console.log("Connected to the database successfully", db_status);
+
+} catch (error) {
+  console.log("Unable to connect to database", error);
+}
+
 // Listen server
 app_server.listen( app_server.get('port'), () => {
   console.log("Server is running in port: ", app_server.get('port'));
